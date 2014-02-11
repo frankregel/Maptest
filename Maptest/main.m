95eb3a7029f837c8a8b45f789729a5226871959c
frankregel <frank.regel@gmail.com>
2014-02-11 10:49:44 +0100

    Initial Commit

---
+++ Maptest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Maptest
+//
+//  Created by Frank on 11.02.14.
+//  Copyright (c) 2014 Frank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
